2137ea202e06c3cac438538aab548b63ff6dfbbf
rich <rkba2@outlook.com>
2023-08-15 17:50:31 -0400

    3 new files

---
+++ primes.py
@@ -0,0 +1,12 @@
+def find_primes(max_int):
+    number_set = [True] * max_int
+    print(number_set)
+    number_set[0] = number_set[1] = False
+    
+    for number, is_prime in enumerate(number_set):
+        print(number, isPrime)
+        if is_prime and number 
+    
+
+find_primes(25)    
+    
---
+++ sum_array.py
@@ -0,0 +1,9 @@
+ar = [1, 2, 3]
+
+sum = 0
+
+for item in ar:
+    sum += item
+
+print(sum)
+
